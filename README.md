8a2572fa7a8d4cf0ded92bae17554bac1fde306c
Pavel Popov <pavel@findhotel.net>
2018-03-26 16:00:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# tsdin - replace all timestamps in stdin to human readable dates
+
+
+Usage:
+
+    echo "Ts is: 1522072756, 1522071756" | tsdin
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var tsRegexp = regexp.MustCompile(`\b(15\d{8})(\.)?(\d{1,3})?\b`)
+
+func getTimeString(ts string) string {
+	i, err := strconv.ParseInt(ts, 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tm := time.Unix(i, 0)
+	return tm.Format(`"2006-01-02 15:04:05 MST"`)
+}
+
+func main() {
+	bytes, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal("Reading stdin failed", err)
+	}
+	stdin := string(bytes)
+
+	result := tsRegexp.FindAllStringSubmatch(stdin, -1)
+	for _, item := range result {
+		ts := item[1]
+		timeString := getTimeString(ts)
+		stdin = strings.Replace(string(stdin), item[0], timeString, -1)
+	}
+
+	fmt.Print(stdin)
+}
