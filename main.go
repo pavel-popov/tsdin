055c5d70e8abb9f873814a20e9a33113af65cb92
Pavel Popov <pavel@findhotel.net>
2018-10-02 20:45:07 +0200

    Add reverse flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	golog "log"
@@ -11,16 +12,36 @@ import (
 	"time"
 )
 
-var tsRegexp = regexp.MustCompile(`\b(15\d{8})(\.)?(\d{1,3})?\b`)
-var log = golog.New(os.Stderr, "tsdin", golog.LstdFlags)
+var log = golog.New(os.Stderr, "tsdin ", golog.LstdFlags)
 
-func getTimeString(ts string) string {
+var unixRegexp = regexp.MustCompile(`\b(15\d{8})(\.)?(\d{1,3})?\b`)
+
+func unixToString(ts string) string {
 	i, err := strconv.ParseInt(ts, 10, 64)
 	if err != nil {
 		log.Fatal(err)
 	}
 	tm := time.Unix(i, 0)
-	return tm.Format(`2006-01-02T15:04:05-07:00`)
+	return tm.Format(timeLayout)
+}
+
+var isoRegexp = regexp.MustCompile(`("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}?Z")`)
+
+func isoToString(ts string) string {
+	t, err := time.Parse(timeLayout, ts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strconv.Itoa(int(t.Unix()))
+}
+
+var timeLayout string
+var fromISOtoUnix bool
+
+func init() {
+	flag.StringVar(&timeLayout, "layout", `"`+time.RFC3339+`"`, "time layout")
+	flag.BoolVar(&fromISOtoUnix, "reverse", false, "turn ISO time into unix timestamp")
+	flag.Parse()
 }
 
 func main() {
@@ -30,12 +51,20 @@ func main() {
 	}
 	stdin := string(bytes)
 
-	result := tsRegexp.FindAllStringSubmatch(stdin, -1)
-	for _, item := range result {
-		ts := item[1]
-		timeString := getTimeString(ts)
-		stdin = strings.Replace(string(stdin), item[0], timeString, 1)
+	re := unixRegexp
+	f := unixToString
+
+	if fromISOtoUnix {
+		re = isoRegexp
+		f = isoToString
 	}
 
+	result := re.FindAllStringSubmatch(stdin, -1)
+	for _, item := range result {
+		token := item[1]
+		out := f(token)
+		stdin = strings.Replace(string(stdin), item[0], out, 1)
+	}
 	fmt.Print(stdin)
+
 }
